fdd3127c8f0ae293b26bb2b0967b9a20ddc3ba53
velopace <velopace@outlook.com>
2023-06-16 09:34:11 -0500

    post category form

---
+++ Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+	public class CreateModel : PageModel
+    {
+        public Category Category { get; set; }
+
+        public void OnGet()
+        {
+        }
+    }
+}
