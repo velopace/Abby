b172c6f27dfc93f7fa72da7ff47856e8a4d5c353
velopace <velopace@outlook.com>
2023-06-16 07:11:31 -0500

    db setting

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using AbbyWeb.Model;
+
+namespace AbbyWeb.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Category> Category { get; set; }
+	}
+}
+
---
+++ Model/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Model
+{
+	public class Category
+	{
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
+
