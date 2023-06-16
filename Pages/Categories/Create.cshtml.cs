be0c43b6c99b6865b44655fd2dbfddec55600e6c
velopace <velopace@outlook.com>
2023-06-16 09:41:23 -0500

    post category action

--- Pages/Categories/Create.cshtml.cs
+++ Pages/Categories/Create.cshtml.cs
@@ -2,18 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AbbyWeb.Data;
 using AbbyWeb.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace AbbyWeb.Pages.Categories
 {
+    [BindProperties]
 	public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
         public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost(Category category)
+        {
+            await _db.Category.AddAsync(category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
