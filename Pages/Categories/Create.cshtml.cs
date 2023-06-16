36bfe36b2364327ac5fcc83d17e248bbdcbc3ad1
velopace <velopace@outlook.com>
2023-06-16 10:44:45 -0500

    update server side validation

--- Pages/Categories/Create.cshtml.cs
+++ Pages/Categories/Create.cshtml.cs
@@ -29,7 +29,7 @@ namespace AbbyWeb.Pages.Categories
         {
             if (Category.Name == Category.DisplayOrder.ToString())
             {
-                ModelState.AddModelError(string.Empty, "The DisplayOrder cannot exactly match the Name");
+                ModelState.AddModelError(string.Empty, "The Display Order cannot exactly match the Name");
             }
             if (ModelState.IsValid)
             {
